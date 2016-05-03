ef73a7389c030cbcb08d51a8bd4682a987615de3
chickendaisuki <chickendaisuki@chickendaisuki>
2016-05-03 12:14:23 +0900

    Canvasへの描き込みを同期できるようにした

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ io.on('connection', function(socket){
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
-  
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+
+  socket.on('send drawing', function(pos){
+  	io.emit('receive drawing', pos);
   });
 });
 
